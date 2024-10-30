c122f3550d6b89207e5649d496ea993088c7f815
MisbahAnsar <misbaansari444@gmail.com>
2024-10-30 19:19:58 +0530

    added todo functionality

--- server/controllers/todoController.js
+++ server/controllers/todoController.js
@@ -1,12 +1,13 @@
 const TodoList = require('../models/todoSchema');
 
-const CreateTodo = async (req, res) => {
+exports.CreateTodo = async (req, res) => {
   try {
       const newPost = new TodoList({ 
           owner: req.user.id, 
           title: req.body.title,
           content: req.body.content,
       });
+      await newPost.save();
       res.status(201).json(newPost);
   } catch (error) {
       console.error(error);
@@ -14,5 +15,23 @@ const CreateTodo = async (req, res) => {
   }
 };
 
+exports.removeTodo = async (req, res) => {
+  try {
+    const todolistid = req.user.id;
+    const id = req.params;
+    
+    const deletedTodo = await TodoList.findOneAndDelete({ _id: id, owner: todolistid });
+    if (!deletedTodo) {
+      return res.status(404).json({ message: "Todo not found" });
+    }
+    res.status(200).json({
+      message: "todo deleted successfully"
+    })
+  } catch (error){
+    console.error(error);
+    res.status(500).json({
+      message: 'Cannot delete post'
+    })
+  }
+}
 
-module.exports = CreateTodo
--- server/routes/todoRoutes.js
+++ server/routes/todoRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const CreateTodo = require('../controllers/todoController');
+const { CreateTodo, removeTodo } = require('../controllers/todoController');
 const router = express.Router();
 const { protect } = require('../middlewares/protect')
 
 router.post('/create', protect, CreateTodo);
+router.delete('/remove/:todolistid', protect, removeTodo)
 
 module.exports = router;
