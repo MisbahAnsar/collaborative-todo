eb77ccfc3e1605e6e3a6ae7650db6d651b7bff9d
MisbahAnsar <misbaansari444@gmail.com>
2024-10-30 23:59:04 +0530

    functionality for collaborative tasks

--- server/controllers/taskControllers.js
+++ server/controllers/taskControllers.js
@@ -1,9 +1,16 @@
-// const Task = require('../models/taskSchema');
+const Task = require('../models/taskSchema');
 
-// const createTask = async( req,res ) => {
-//     const token = req.token;
-
-//     if(token){
-
-//     }
-// }
+const createTask = async( req,res ) => {
+    try {
+        const newPost = new TodoList({
+          owner: req.user.id,
+          title: req.body.title,
+          content: req.body.content,
+        });
+        await newPost.save();
+        res.status(201).json(newPost);
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Error creating post" });
+      }
+}
