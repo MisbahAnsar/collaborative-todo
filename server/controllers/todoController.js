e0ae3fdd3c21c65dc4a86b14b20e401d22cfa4a4
MisbahAnsar <misbaansari444@gmail.com>
2024-10-30 22:23:22 +0530

    added updating functionality

--- server/controllers/todoController.js
+++ server/controllers/todoController.js
@@ -1,37 +1,76 @@
-const TodoList = require('../models/todoSchema');
+const TodoList = require("../models/todoSchema");
 
 exports.CreateTodo = async (req, res) => {
   try {
-      const newPost = new TodoList({ 
-          owner: req.user.id, 
-          title: req.body.title,
-          content: req.body.content,
-      });
-      await newPost.save();
-      res.status(201).json(newPost);
+    const newPost = new TodoList({
+      owner: req.user.id,
+      title: req.body.title,
+      content: req.body.content,
+    });
+    await newPost.save();
+    res.status(201).json(newPost);
   } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Error creating post' });
+    console.error(error);
+    res.status(500).json({ message: "Error creating post" });
   }
 };
 
 exports.removeTodo = async (req, res) => {
   try {
-    const todolistid = req.user.id;
-    const id = req.params;
-    
-    const deletedTodo = await TodoList.findOneAndDelete({ _id: id, owner: todolistid });
+    const todolistid = req.user.id; // User ID from the authenticated request
+    const id = req.params.todolistid; // Extracting todo ID from request params
+
+    console.log(
+      `Attempting to delete todo with ID: ${id} by owner: ${todolistid}`
+    );
+
+    const deletedTodo = await TodoList.findOneAndDelete({
+      _id: id,
+      owner: todolistid,
+    });
+
     if (!deletedTodo) {
       return res.status(404).json({ message: "Todo not found" });
     }
+
     res.status(200).json({
-      message: "todo deleted successfully"
-    })
-  } catch (error){
+      message: "Todo deleted successfully",
+    });
+  } catch (error) {
     console.error(error);
     res.status(500).json({
-      message: 'Cannot delete post'
-    })
+      message: "Cannot delete post",
+    });
   }
-}
+};
 
+exports.updateTodo = async (req, res) => {
+  try {
+    const todoId = req.user.id;
+    const id = req.params.todoId;
+    const { title, content }  = req.body
+
+    console.log(`Attempting to delete todo with ID: ${id} by owner: ${todoId}`);
+    
+    const updateTodo = await TodoList.findByIdAndUpdate(
+      { _id: id, owner: todoId },
+      { title: title, content: content },
+      { new: true }
+    );
+
+    if (!updateTodo) {
+      return res.status(404).json({
+        message: "Todo not found",
+      });
+    }
+
+    res.status(200).json({
+      message: "Todo updated successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Cannot update post",
+    });
+  }
+};
--- server/routes/todoRoutes.js
+++ server/routes/todoRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { CreateTodo, removeTodo } = require('../controllers/todoController');
+const { CreateTodo, removeTodo, updateTodo } = require('../controllers/todoController');
 const router = express.Router();
 const { protect } = require('../middlewares/protect')
 
 router.post('/create', protect, CreateTodo);
 router.delete('/remove/:todolistid', protect, removeTodo)
+router.put('/update/:todoId', protect, updateTodo)
 
 module.exports = router;
